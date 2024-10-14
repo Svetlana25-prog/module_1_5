0ae15a20e13a4c24d79d9b8315aad6c86d703ff1
Svetlana <skyfall12@mail.ru>
2024-10-14 15:14:48 +0300

    module_1_5

---
+++ main.py
@@ -0,0 +1,9 @@
+immutable_var_tuple=(1,2,3,"Name",)
+print(immutable_var_tuple)
+#immutable_var_tuple[0] = 10
+mutable_list=[ "cat", "dog", "pig"]
+print(mutable_list)
+mutable_list[0] = 15
+mutable_list[2] = 30
+print(mutable_list)
+
